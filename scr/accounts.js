e4977b04a37b24659323fd3db6206e8f5f747c5e
Dount-Holestien <jameselicke@gmail.com>
2025-03-27 13:42:19 -0400

    Update accounts.js

--- scr/accounts.js
+++ scr/accounts.js
@@ -1,16 +1,15 @@
 function findAccountById(accounts, id) {
-  // YOUR SOLUTION HERE
-  // Hint: You can use the [`find()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find) method here. 
+  return accounts.find((account) => account.id === id);
 }
 
 function sortAccountsByLastName(accounts) {
-  // YOUR SOLUTION HERE
-  // Hint: You can use the [`sort()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort) method here. 
+  return accounts.sort((a, b) =>
+    a.name.last.toLowerCase() > b.name.last.toLowerCase() ? 1 : -1
+  );
 }
 
 function getAccountFullNames(accounts) {
-  // YOUR SOLUTION HERE
-  // Hint: You can use the [`map()`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map) method here.
+  return accounts.map((account) => `${account.name.first} ${account.name.last}`);
 }
 
 // NOTE: YOU DON'T HAVE TO EDIT THE FUNCTIONS BELOW
@@ -19,7 +18,6 @@ function getTotalNumberOfBorrows(account, books) {
     const count = book.borrows.reduce((borrowAcc, borrow) => {
       return borrow.id === account.id ? borrowAcc + 1 : borrowAcc;
     }, 0);
-
     return acc + count;
   }, 0);
 }
